ad451ac42770119d56efbd7b359116c374cc99a8
CluadiaSantana <82841306+CluadiaSantana@users.noreply.github.com>
2022-11-22 00:02:22 -0600

    login done

--- proyecto/lib/main.dart
+++ proyecto/lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:proyecto/pages/bloc/tutoapp_bloc.dart';
+import 'package:proyecto/pages/login/bloc/auth_bloc.dart';
 import 'package:proyecto/pages/login/login.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-void main() =>
-    runApp(BlocProvider(create: (context) => TutoappBloc(), child: MyApp()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MultiBlocProvider(providers: [
+    BlocProvider(
+      create: (context) => AuthBloc()..add(VerifyAuthEvent()),
+    ),
+    BlocProvider(create: (context) => TutoappBloc()),
+  ], child: MyApp()));
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
